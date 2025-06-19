3a4cc6ed600d9b0f4dc2359530d50a3cc738f056
pawelbugiel <pawel.bugiel.7@gmail.com>
2025-06-19 20:19:55 +0200

    BE: 1. ProductServiceImpl.java - add Lombok @Slf4j to provide logger. Add basic logging messages. 2. logback-spring.xml - add file to gain more control over logging configuration.

--- backend/src/main/java/com/pawelbugiel/wastenofood/services/ProductServiceImpl.java
+++ backend/src/main/java/com/pawelbugiel/wastenofood/services/ProductServiceImpl.java
@@ -8,8 +8,7 @@ import com.pawelbugiel.wastenofood.models.Product;
 import com.pawelbugiel.wastenofood.repositories.ProductRepository;
 import com.pawelbugiel.wastenofood.validators.ObjectValidator;
 import com.pawelbugiel.wastenofood.validators.PageableValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -20,14 +19,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
     private final PageableValidator pageableValidator;
     private final ObjectValidator<ProductRequest> objectValidator;
-
-    private final static Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
 
     public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper, PageableValidator pageValidator, ObjectValidator<ProductRequest> objectValidator) {
         this.productRepository = productRepository;
@@ -43,6 +41,7 @@ public class ProductServiceImpl implements ProductService {
     public ProductResponse createProduct(ProductRequest productRequest) {
 
         objectValidator.validate(productRequest);
+        log.info("Attempting to create or update product with name: [{}]", productRequest.getName()); // tu jest problem 'null
 
         Optional<Product> existingProduct = productRepository
                 .findByNameAndExpiryDate(productRequest.getName(), productRequest.getExpiryDate());
@@ -50,10 +49,14 @@ public class ProductServiceImpl implements ProductService {
         Product savedProduct;
 
         if (existingProduct.isEmpty()) {
+            log.info("Product with name [{}] does not exist. Creating a new one.", productRequest.getName());
+
             Product passedProduct = productMapper.toProduct(productRequest);
             savedProduct = productRepository.save(passedProduct);
         } else {
             Product actualProduct = existingProduct.get();
+
+            log.info("Product with name [{}] already exists (ID: {}). Updating quantity.", actualProduct.getName(), actualProduct.getId());
 
             actualProduct.setQuantity(actualProduct.getQuantity() + productRequest.getQuantity());
             savedProduct = productRepository.save(actualProduct);
@@ -64,37 +67,53 @@ public class ProductServiceImpl implements ProductService {
     //************** READ *************
     @Override
     public Page<ProductResponse> findAllProducts(int page, Integer pageSize, String sortBy, Sort.Direction sortDirection) {
+        log.info("Fetching all products - page: {}, size: {}, sort: {},{}", page, pageSize, sortBy, sortDirection);
 
         Pageable pageable = createPageable(page, pageSize, sortBy, sortDirection);
 
-        return productRepository
+        Page<ProductResponse> productPage = productRepository
                 .findAll(pageable)
                 .map(productMapper::toProductResponse);
+
+        log.debug("Found {} products in total.", productPage.getTotalElements());
+
+        return productPage;
     }
 
     @Override
     public ProductResponse findProductById(UUID id) {
+        log.info("Fetching product with ID: {}", id);
+
         Product product = findProductByIdOrThrow(id);
+
         return productMapper.toProductResponse(product);
     }
 
     private Product findProductByIdOrThrow(UUID id) {
+        log.debug("Searching for product with ID: {}", id);
+
         return productRepository
                 .findById(id)
-                .orElseThrow(() -> new ProductNotFoundException(id));
+                .orElseThrow(() -> {
+                    log.error("Product with ID: {} not found. Throwing ProductNotFoundException.", id);
+                    return new ProductNotFoundException(id);
+                });
     }
 
     @Override
     public Page<ProductResponse> findProductsByPartialName(
             String partialName, int page, Integer pageSize, String sortBy, Sort.Direction sortDirection) {
+        log.info("Finding products by partial name: [{}] - page: {}, size: {}", partialName, page, pageSize);
 
         Pageable pageable = createPageable(page, pageSize, sortBy, sortDirection);
 
-        Page<Product> products = productRepository
-                .findByPartialName(partialName, pageable);
-
-        return products
+        Page<ProductResponse> products = productRepository
+                .findByPartialName(partialName, pageable)
                 .map(productMapper::toProductResponse);
+
+        log.debug("Found {} products matching the partial name.", products.getTotalElements());
+        return products;
+
     }
 
     private Pageable createPageable(int page, Integer pageSize, String sortBy, Sort.Direction sortDirection) {
@@ -105,42 +124,50 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Page<ProductResponse> findProductsWithExpiredDate(
             int page, Integer pageSize, String sortBy, Sort.Direction sortDirection) {
+        log.info("Finding products with expired date - page: {}, size: {}", page, pageSize);
 
         Pageable pageable = createPageable(page, pageSize, sortBy, sortDirection);
 
-        Page<Product> expiredProductsPage = productRepository
-                .findWithExpiredDate(pageable);
-
-        return expiredProductsPage
+        Page<ProductResponse> expiredProductsPage = productRepository
+                .findWithExpiredDate(pageable)
                 .map(productMapper::toProductResponse);
+
+        log.debug("Found {} expired products.", expiredProductsPage.getTotalElements());
+        return expiredProductsPage;
+
     }
 
 //************** UPDATE *************
 
     @Override
+    @Transactional
     public ProductResponse updateProduct(UUID id, ProductRequest productRequest) {
-
+        log.info("Attempting to update product with ID: {}", id);
         objectValidator.validate(productRequest);
 
         Product productToUpdate = findProductByIdOrThrow(id);
+        log.debug("Product to update found. Applying changes for product: {}", productToUpdate.getName());
 
         productMapper.updateProductFromRequest(productRequest, productToUpdate);
 
         Product savedProduct = productRepository.save(productToUpdate);
 
+        log.info("Successfully updated product with ID: {}", savedProduct.getId());
         return productMapper.toProductResponse(savedProduct);
     }
 
 //************** DELETE *************
 
     @Override
+    @Transactional
     public ProductResponse deleteProductById(UUID id) {
+        log.info("Attempting to delete product with ID: {}", id);
         Product foundProduct = findProductByIdOrThrow(id);
-
         ProductResponse productResponse = productMapper.toProductResponse(foundProduct);
 
         productRepository.deleteById(id);
 
+        log.info("Successfully deleted product with ID: {} and name: [{}]", id, foundProduct.getName());
         return productResponse;
     }
 }
