f5d7e6ae68d16a712351e2321dc9bd751f1ac56c
pawelbugiel <pawel.bugiel.7@gmail.com>
2025-07-09 11:34:03 +0200

    DEV: 1. init.sql   - regenerate file with --hex-blob to support UUID binary types. 2. .env   - create file to store the database password securely. 3. docker-compose.yml   - update to use the MYSQL_ROOT_PASSWORD variable. 4. .gitignore   - add .env to file to prevent secrets from being committed.

---
+++ database/init.sql
@@ -0,0 +1,130 @@
+﻿-- MySQL dump 10.13  Distrib 9.2.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: fresh_food
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` binary(16) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `expiry_date` date NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (0x0DFCBA69D02A440892E5D58C8FB4038A,'Beef','2025-08-07',2),(0x13AC0270BEBF4B2493276EA875708024,'Agrest','2025-06-13',23),(0x2F11CBA9B65E41CAB4DF1CA7DA008B31,'Snickers','2025-09-09',5),(0x3E7294581C7E4A13B850DC7AAEE739BC,'nowy produkt','2025-06-13',22),(0x4150C6325B6A46EBB4991E67194FF7AC,'Chicken','2025-07-29',6),(0x6AEFF4C1F46A49D59ED7EF9FF400531C,'Cheese','2025-06-13',1),(0x836B0187FDFE4BB98703F5A404F17262,'Strawberry','2025-06-02',64),(0x8A83E007FEA949D2A7A163EC94AF1079,'oranges','2026-05-08',2),(0xA7B160DA3B6A4DF9B05F3BF4E3993AA8,'Apple','2025-07-31',791),(0xB4E8CBC2D0F344019D96D6BFE98A30C5,'Banana','2025-06-22',6),(0xBA4DF929385845139E67156594ACE629,'Cocomelon','2025-06-26',212),(0xC3A56EF13ECA4838B9435F00C9FED0F9,'Milk','2025-07-16',4455),(0xC54715EFAC7B43A6AB6D74F57CE0DFB8,'orzechy laskowe','2025-06-13',12),(0xC924ABF777B449FBA692A2F36C9E59AC,'DEVELOP BASE','2025-10-09',24),(0xDB946605160843DFB203638E9966DC90,'Oil','2026-05-29',51);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (3,'ROLE_ADMIN'),(4,'ROLE_ENDUSER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+  `user_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (31,3),(32,4);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (31,'$2a$10$ool4ri/A5T8ao2Ie8UGy3eSA5fGITAcxGo9SyHVHhM5ss2r9zD/oy','admin@admin'),(32,'$2a$10$25e5qA2ZcAjnfZ5S3kZrRe/KTK85nCwXv.UkcRTue8IJxQsLUMZba','user@user');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-09  8:58:29
