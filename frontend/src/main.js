8ba648c1213b49ddfa7a50bad7b6caac8af16193
pawelbugiel <pawel.bugiel.7@gmail.com>
2025-07-12 10:39:46 +0200

    FE: Configure project for Vue 3 with BootstrapVueNext

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,18 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import BootstrapVueNext from 'bootstrap-vue-next';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import './styles/global.css';
 
 const app = createApp(App);
 const pinia = createPinia();
 
+// Rejestrujemy w aplikacji Pinia, Router i BootstrapVue
 app.use(pinia);
 app.use(router);
+app.use(BootstrapVueNext);
+
 app.mount('#app');
